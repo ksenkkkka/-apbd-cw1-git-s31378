1d698558a2133a96f874524181358b4fed5543d7
Kseniya Shakhnovich <s31378@pjwstk.edu.pl>
2026-03-10 11:28:59 +0100

    Add user input parsing Task2  Commit 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        string input1 = Console.ReadLine();
+
+        Console.WriteLine("Enter second number:");
+        string input2 = Console.ReadLine();
+
+        int number1 = int.Parse(input1);
+        int number2 = int.Parse(input2);
+
+        Console.WriteLine("Numbers received: " + number1 + " and " + number2);
+    }
+}
