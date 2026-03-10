3b51af663b2e13a6f86c75795e0c1ee3ba95618a
Kseniya Shakhnovich <s31378@pjwstk.edu.pl>
2026-03-10 11:40:26 +0100

    Add CalculateAverage method tASK 4

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -4,4 +4,16 @@
     {
         return a + b;
     }
+    
+    public static double CalculateAverage(int[] values)
+    {
+        int sum = 0;
+
+        foreach (int v in values)
+        {
+            sum += v;
+        }
+
+        return (double)sum / values.Length;
+    }
 }
