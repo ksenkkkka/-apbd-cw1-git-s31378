e308b9540fef88cc64a2eb6626175ced0fd7f690
Kseniya Shakhnovich <s31378@pjwstk.edu.pl>
2026-03-18 00:25:01 +0300

    Change validation message in feature-conflict

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ class Program
 
         if (!int.TryParse(input1, out int number1) || !int.TryParse(input2, out int number2))
         {
-            Console.WriteLine("Error: please enter valid numbers");
+            Console.WriteLine("Feature branch: invalid input");
             return;
         }
 
