a1ac0453b3b98ca7789b8cd4df5b35458a257471
Kseniya Shakhnovich <s31378@pjwstk.edu.pl>
2026-03-18 00:19:06 +0300

    Add CalculateMin method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -34,4 +34,16 @@
 
         return max;
     }
+    public static int CalculateMin(int[] values)
+    {
+        int min = values[0];
+
+        foreach (int v in values)
+        {
+            if (v < min)
+                min = v;
+        }
+
+        return min;
+    }
 }
