7628b108cb2466a05566630a04cc0d683550e0eb
Kseniya Shakhnovich <s31378@pjwstk.edu.pl>
2026-03-18 00:12:09 +0300

    Add CalculateMax method TASK5

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -19,4 +19,19 @@
 
         return (double)sum / values.Length;
     }
+    public static int CalculateMax(int[] values)
+    {
+        if (values == null || values.Length == 0)
+            return 0;
+
+        int max = values[0];
+
+        foreach (int v in values)
+        {
+            if (v > max)
+                max = v;
+        }
+
+        return max;
+    }
 }
